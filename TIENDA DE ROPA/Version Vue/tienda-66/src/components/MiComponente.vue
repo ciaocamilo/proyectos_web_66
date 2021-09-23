dbe667753e4d68ef3683c74ae04b5ecc521cfa68
Camilo A <ing_camiloacg@hotmail.com>
2021-09-23 10:49:47 -0500

    Crea proyecto VUE

---
+++ TIENDA DE ROPA/Version Vue/tienda-66/src/components/MiComponente.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Bienvenido {{ cliente.nombre }} </h1>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+       return {
+           cliente: { nombre: 'Juan', apellido: 'Castañeda' },
+           cantidad: 0
+       }
+    }
+}
+</script>
