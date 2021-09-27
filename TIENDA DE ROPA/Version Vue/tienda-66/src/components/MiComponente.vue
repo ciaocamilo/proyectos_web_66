9b476417d2e9a52789fda47c1defcfa7bace9b6c
Camilo A <ing_camiloacg@hotmail.com>
2021-09-27 09:17:50 -0500

    Agrega data y methods a MiComponente. Uso de v-if, v-show y v-for

--- TIENDA DE ROPA/Version Vue/tienda-66/src/components/MiComponente.vue
+++ TIENDA DE ROPA/Version Vue/tienda-66/src/components/MiComponente.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="container">
         <h1>Bienvenido {{ cliente.nombre }} </h1>
+        <div class="container">
+            <div class="row">
+                <input type="text" v-model="cliente.nombre">
+                <input type="number" v-model="cantidad">
+            </div>
+            <div class="row">
+                <h3 v-if="cantidad > 0" >Número mayor a cero</h3>
+                <p v-else-if="cantidad === 0">Número igual a cero</p>
+                <h2 v-else>Número negativo</h2>
+            </div>
+            <div class="row">
+                <!-- Este es el v-show -->
+                <h2 v-show="cantidad > 0" >Número positivo (V-show)</h2>
+            </div>
+        </div>
+        <!--
+        <div class="container">
+            {{ productos }}
+        </div>
+        -->
+        <div class="container">
+            <div v-for="producto in productos" :key="producto.id">
+                <h1>Producto: {{producto.nombre}} </h1>
+                <h2>Precio: {{ producto.precio }} </h2>
+                <button type="button" @click="mostrarPrecioAcumulado(producto.precio)">Agregar</button>
+            </div>
+        </div>
+        <div class="container">
+            <h3> <b>PRECIO TOTAL = </b>{{ precioAcumulado }} </h3>
+        </div>
     </div>
 </template>
 <script>
@@ -8,8 +38,37 @@ export default {
     data () {
        return {
            cliente: { nombre: 'Juan', apellido: 'Castañeda' },
-           cantidad: 0
+           cantidad: 0,
+           productos: [
+               {
+                   id: 1,
+                   nombre: 'Camiseta',
+                   precio: 25000
+               },
+               {
+                   id: 2,
+                   nombre: 'Vestido',
+                   precio: 150000
+               },
+               {
+                   id: 3,
+                   nombre: 'Pantalón',
+                   precio: 120000
+               }
+           ],
+           precioAcumulado: 0
        }
+    },
+    methods: {
+        mostrarPrecioAcumulado (precioProducto) {
+            // Ejemplo //
+            let variable1 = 'mi_variable'
+            const mi_constante = 5
+            console.log(variable1)
+            console.log(mi_constante)
+            ////
+            this.precioAcumulado = this.precioAcumulado + precioProducto
+        }
     }
 }
 </script>
